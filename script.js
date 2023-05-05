74d8349c30ccff939b3b3ce68734a02123a7acb6
VladSimonyan <129136870+VladSimonyan@users.noreply.github.com>
2023-05-04 19:05:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
+$(document).ready(function () {
+   
+   
+    let currentDay = moment().format('LLLL');
+    let currentTime = (moment().hour());
+
+    
+    for (key = 0; key < localStorage.length; key++){
+        timeID = localStorage.key(key);
+        timeValue = localStorage.getItem(timeID);
+        $("#"+timeID).val(timeValue);
+
+
+    }
+
+   
+    $("#currentDay").append(currentDay);
+
+    $("input").each(function () {
+        var item = parseInt($(this).attr("time"));
+  
+        console.log(item)
+        
+        if (item < currentTime) {
+            $(this).addClass("past");
+        }
+
+
+        if (item > currentTime) {
+            $(this).addClass("present")
+        }
+
+        if (item === currentTime) {
+            $(this).addClass("future")
+        }
+    });
+
+
+
+    $("button").on("click", function () {
+        
+        inputID = $(this).attr("input-id");
+        inputEl = $(document.getElementById(inputID))
+
+
+
+        
+
+        calendarEvent = inputEl.val();
+        console.log($(this).val())
+        console.log($(this).parent)
+        localStorage.setItem(inputEl.attr("id"), calendarEvent);
+
+    })
+
+
+});
 
